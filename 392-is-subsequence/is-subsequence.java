15105113c2e39cde61807d3a6b0031dc65ef9dfa
Yashwanth B K <132933255+Yashwanth-Gowda-BK@users.noreply.github.com>
2024-09-09 16:12:49 +0530

    Time: 2 ms (58.47%) | Memory: 41.2 MB (88.32%) - LeetSync

--- 392-is-subsequence/is-subsequence.java
+++ 392-is-subsequence/is-subsequence.java
@@ -3,11 +3,11 @@ class Solution {
 
         if(s.length() == 0) return true;
         if(t.length() == 0) return false;
-        char[] s_char = s.toCharArray();
-        char[] t_char = t.toCharArray();
+        // char[] s_char = s.toCharArray();
+        // char[] t_char = t.toCharArray();
 
-        s = new String(s_char);
-        t = new String(t_char);
+        // s = new String(s_char);
+        // t = new String(t_char);
         int i = 0;
         int j = 0;
         while(j < t.length() && i<s.length()){
