5399c9881c3f45c02bda5841b4b034996da577f7
Yashwanth B K <132933255+Yashwanth-Gowda-BK@users.noreply.github.com>
2024-09-12 20:41:32 +0530

    Time: 4 ms (72.95%) | Memory: 44.9 MB (13.42%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,13 +1,13 @@
+import java.util.Arrays;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
-    char arr1[] = s.toCharArray();
-    char arr2[] = t.toCharArray();
-    Arrays.sort(arr1);
-    Arrays.sort(arr2);
-    
-    s = new String(arr1);
-    t = new String(arr2);
-
-    return s.equals(t);
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        
+        return Arrays.equals(sChars, tChars);
     }
 }
