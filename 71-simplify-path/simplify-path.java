fe0da3fd000f44cf06d305fe1fe2f83cd67ffdce
Yashwanth B K <132933255+Yashwanth-Gowda-BK@users.noreply.github.com>
2024-09-20 19:59:59 +0530

    Time: 4 ms (86.58%) | Memory: 42.8 MB (96.50%) - LeetSync

---
+++ 71-simplify-path/simplify-path.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+class Solution {
+    public String simplifyPath(String path) {
+        String[] parts = path.split("/");
+
+        Stack<String> stack = new Stack<>();
+
+        for (String part : parts) {
+            if (part.isEmpty() || part.equals(".")) {
+                continue;
+            }
+            if (part.equals("..")) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            } else {
+                stack.push(part);
+            }
+        }
+
+        if (stack.isEmpty()) {
+            return "/";
+        }
+
+        StringBuilder res = new StringBuilder();
+        for (String dir : stack) {
+            res.append("/").append(dir);
+        }
+
+        return res.toString();
+    }
+}
