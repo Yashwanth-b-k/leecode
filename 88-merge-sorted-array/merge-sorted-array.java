9bc31bfd942309ec179a92cba9b6ecea6294a9d3
Yashwanth B K <132933255+Yashwanth-b-k@users.noreply.github.com>
2025-04-16 11:47:45 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (89.39%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,10 +1,26 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int count = 0;
-        for(int i=m; i<m+n; i++){
-            nums1[i] = nums2[count];
-            count++;
+        int i = m - 1;        // pointer for the end of actual nums1 elements
+        int j = n - 1;        // pointer for the end of nums2
+        int k = m + n - 1;    // pointer for the end of nums1 (full size)
+
+        // Start from the end and move backwards
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
         }
-        Arrays.sort(nums1);
+
+        // If nums2 still has elements left
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
     }
 }
